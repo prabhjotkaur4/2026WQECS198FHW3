abf35cdf5c3af71dd9254949acfbb38c3e638ea2
prabhjotkaur4 <pjkkaur@ucdavis.edu>
2026-02-27 23:04:51 -0800

    Added test cases to test_foo_bar_baz.py

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -2,4 +2,17 @@ import pytest
 
 from foo_bar_baz import foo_bar_baz
 
-#Add testcases Here
+def test_3Multiples():
+    assert foo_bar_baz(3) == "1 2 Foo"
+
+def test_5Multiples():
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+
+def test_BothMultiples():
+    assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+
+def test_NonMultiple():
+    assert foo_bar_baz(1) == "1"
+
+def test_Empty():
+    assert foo_bar_baz(0) == ""
