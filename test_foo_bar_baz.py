99e8aa0e0874f185ab3f9829fab631ab88530bc9
prabhjotkaur4 <pjkkaur@ucdavis.edu>
2026-02-27 23:16:34 -0800

    Added test cases for negative values

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -4,9 +4,13 @@ from foo_bar_baz import foo_bar_baz
 
 def test_3Multiples():
     assert foo_bar_baz(3) == "1 2 Foo"
+    assert foo_bar_baz(6) == "1 2 Foo 4 Bar Foo"
+    assert foo_bar_baz(9) == "1 2 Foo 4 Bar Foo 7 8 Foo"
 
 def test_5Multiples():
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+    assert foo_bar_baz(10) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar"
+
 
 def test_BothMultiples():
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
@@ -16,3 +20,7 @@ def test_NonMultiple():
 
 def test_Empty():
     assert foo_bar_baz(0) == ""
+
+def test_NegativeMultiple():
+    assert foo_bar_baz(-3) == ""
+    assert foo_bar_baz(-5) == ""
